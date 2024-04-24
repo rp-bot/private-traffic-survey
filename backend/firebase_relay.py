e5377d3a840c8c584ea7abb92944ceadd6db29be
rpbot_mac <71392285+rp-bot@users.noreply.github.com>
2024-04-24 15:54:38 -0400

    firebase relay complete

--- backend/firebase_relay.py
+++ backend/firebase_relay.py
@@ -6,21 +6,28 @@ from firebase_admin import storage
 import numpy as np
 # import cv2
 import os
-local_directory = os.getcwd()+"/backend"
+import argparse
+
+local_directory = os.getcwd()
 firebase_new_directory = 'new_images'
 firebase_processed_directory = 'processed_images'
 
-currpath = os.listdir(
-    os.getcwd()+'/backend/test_result')
 
 cred = credentials.Certificate(
-    local_directory+"/backend/secrets/private-traffic-survey-firebase-adminsdk-rlpma-b35f59cfc6.json")
+    local_directory+"/secrets/private-traffic-survey-firebase-adminsdk-rlpma-b35f59cfc6.json")
 
 firebase_admin.initialize_app(cred, {
 
     'storageBucket': 'private-traffic-survey.appspot.com'
 
 })  # end of initialization
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('command', choices=[
+                        'download', 'upload'], help='Command to execute: download or upload')
+    return parser.parse_args()
 
 
 def find_latest_file(directory):
@@ -63,15 +70,28 @@ def download_blob(blob, local_file_path):
         print("No files found in the specified bucket directory.")
 
 
-# download
-latest_image_blob = get_latest_image(firebase_new_directory)
-download_blob(latest_image_blob, os.getcwd() +
-              '/backend/client_images/temp.jpg')
-
-# upload
-latest_processed_image = find_latest_file(
-    local_directory+"/backend/test_result")
-upload_to_firebase(latest_processed_image, firebase_processed_directory)
-
 if __name__ == '__main__':
-    pass
+    # download
+    # latest_image_blob = get_latest_image(firebase_new_directory)
+    # download_blob(latest_image_blob, os.getcwd() +
+    #           '/backend/client_images/temp.jpg')
+    args = parse_args()
+
+    if args.command == 'download':
+        # Perform the download operation
+        latest_image_blob = get_latest_image(firebase_new_directory)
+        if latest_image_blob:
+            download_blob(latest_image_blob, os.getcwd() +
+                          '/client_images/temp.jpg')
+        else:
+            print("No latest image found to download.")
+
+    elif args.command == 'upload':
+        # Perform the upload operation
+        latest_processed_image = find_latest_file(
+            os.getcwd() + "/test_result")
+        if latest_processed_image:
+            upload_to_firebase(latest_processed_image,
+                               firebase_processed_directory)
+        else:
+            print("No processed image found to upload.")
