82ddaf98bb346b17f74e2731996d73583903b2a3
rpbot_mac <71392285+rp-bot@users.noreply.github.com>
2024-04-24 15:03:31 -0400

    working pulling

--- backend/zachTest.py
+++ backend/zachTest.py
@@ -1,62 +1,48 @@
-#Get image from storage, use segmentation, and upload new processed image to database
+# Get image from storage, use segmentation, and upload new processed image to database
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import storage
-#from PIL import Image
 import numpy as np
-import cv2
+# import cv2
 import os
+print(os.getcwd())
 
-cred = credentials.Certificate("backend/private-traffic-survey-firebase-adminsdk-rlpma-d297ab2207.json")
+currpath = os.listdir(
+    os.getcwd()+'/backend/test_result')
+
+cred = credentials.Certificate(
+    "backend/private-traffic-survey-firebase-adminsdk-rlpma-b35f59cfc6.json")
 
 firebase_admin.initialize_app(cred, {
 
     'storageBucket': 'private-traffic-survey.appspot.com'
 
-})#end of initialization
-
-bucket = storage.bucket()
-
-#Get all imges in storage
-blobs = list(bucket.list_blobs(prefix = 'new_images/'))
-
-#Get the name of the most recent image
-x = len(blobs)
-x = x - 1
-blob = blobs[x].name
+})  # end of initialization
 
 
-blob2 = bucket.get_blob(blob)
-
-#blob -> array -> image
-#convert blob to string, then string to array of bytes
-arr = np.frombuffer(blob2.download_as_string(), np.uint8)
-
-#get actual image
-img = cv2.imdecode(arr, cv2.COLOR_BGR2BGR555)
-
-#write code to datasets/dat file
-###with open("backend/datasets/data") as outFile:
-###    outFile.write(img)
-
-###Do actual image segmentation
-
-#path to grab image from datasets/test_results
-newPath = os.listdir('backend/test_results')
-
-#grab most recent image name
-y = len(newPath) - 1
-
-#get name of file I want to grab
-newImgId = os.listdir('backend/test_results')[y]
-
-#open file and grab image
-with open ("backend/test_results") as inFile:
-    newImg = inFile.read(newImgId)
+def get_latest_image():
+    bucket = storage.bucket()
+    blobs = bucket.list_blobs() 
+    latest_blob = None
+    latest_name = ""
+    for blob in blobs:
+        if not latest_blob or blob.name > latest_name:
+            latest_blob = blob
+            latest_name = blob.name
+    return latest_blob
 
 
-#show image
-#cv2.imshow('image', newImg)
-#cv2.waitkey(5000)
-#v2.destroyAllWindows()
+def download_blob(blob, local_file_path):
+    if blob:
+        blob.download_to_filename(local_file_path)
+        print(f'Downloaded latest image to {local_file_path}')
+    else:
+        print("No files found in bucket.")
+
+
+latest_image_blob = get_latest_image()
+
+download_blob(latest_image_blob, os.getcwd() +
+              '/backend/downloaded_latest_image.jpg')
+
